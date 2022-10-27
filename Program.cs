6b246260bc26c5ed0584000d642e7fa9a398a0cd
Maxim <maximsokol2288@gmail.com>
2022-10-27 12:57:02 +0300

    MyFCommit

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LESSON_6__LOOPS_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 2
+
+            //int number1 = 1;
+            //int number2 = 6;
+            //int res = 0;
+
+            //for(; number1 < number2; number1++)
+            //{
+            //    if((number1 % 2) != 0)
+            //    {
+            //        Console.WriteLine("Not even!" + number1);
+            //    }
+            //    Console.WriteLine(res += number1);
+            //}
+
+            // Task 3
+
+            // Rectangle
+
+            //for(int i = 0; i < 3; i++)
+            //{
+            //    for(int j = 0; j < 10; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            // equailateral triangle
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            // Task 4
+
+            //int factorial = 5;
+            //int iterator = 0;
+            //int sum = 1;
+
+            //do
+            //{
+            //    ++iterator;
+            //    sum *= iterator;
+
+            //} while (iterator < factorial);
+
+            //Console.WriteLine($"The quantity of possible ways: {sum}");
+        } 
+    }
+}
